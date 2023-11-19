6b163d85a06b3d3f65f20eace1674cea9cd0a64c
jayagz <jayaguantz@gmail.com>
2023-11-19 12:44:11 +0800

    added inheritance

---
+++ src/Animal.java
@@ -0,0 +1,16 @@
+public class Animal {
+    String name;
+    
+    Animal(String name){
+        this.name = name;
+    }
+    
+    void cry(){
+        System.out.println(name  +" is crying");
+    }
+    
+    void dance(){
+        System.out.println(name + " is dancing");
+    }
+    
+}
---
+++ src/Cat.java
@@ -0,0 +1,13 @@
+public class Cat extends Animal{
+   Cat(String name){
+        super(name);
+    }
+    
+    void cry(){
+        System.out.println(name + "is crying");
+    }
+
+   
+    
+    
+}
